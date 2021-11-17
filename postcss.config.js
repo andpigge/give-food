3e966a5e3a4a1a0d3ceb0655fcd25c383f0c6161
Rustam <Rustamaaaa@bk.ru>
2021-11-17 22:32:10 +0300

    feat/Реализация компонента baners

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,21 @@
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const scale = require('./src/shared/helpers/scale')();
 
 module.exports = {
   plugins: [
     autoprefixer,
     cssnano({ preset: 'default' }),
     'postcss-mixins',
-    'postcss-nested'
+    'postcss-nested',
+    [
+      'postcss-functions',
+      {
+        functions: {
+          scale,
+        },
+      }
+    ],
+    'postcss-simple-vars',
   ]
 };
